15871c5e2613163c313566c4bcfb5ada7cce1efe
rpeng35 <rpeng35@users.noreply.github.com>
2020-04-29 22:05:09 +0000

    automated commit by submit50

--- hello.c
+++ hello.c
@@ -4,7 +4,7 @@
 int main(void)
 {
     printf("hello, world\n");
-    string name = get_string("What is your name?\n");
-    printf("hello, %s\n", name);
+    string name = get_string("What is your name?\n");   /*type: string, variable: name*/
+    printf("hello, %s\n", name);                        /*%s is the place holder, name is the additional arugument*/
 }
 
